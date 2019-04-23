d2504e3684e1bce2515d7aa3f8a353fb63a2e092
ZixiangMa <maxxx580@umn.edu>
2019-04-23 15:29:49 -0400

    add crawler file

---
+++ crawler.py
@@ -0,0 +1,16 @@
+"""
+Created on Fri Apr 28 15:13:01 2019
+
+@author: maxxx580
+"""
+
+from bs4 import BeautifulSoup
+from selenium import webdriver
+
+url = input('Enter base url: ')
+
+def scrapper(url):
+    return None
+
+if __name__ == "__main__":
+    scrapper(url)
